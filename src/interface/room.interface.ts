88e2d4fc2b8b099fa257187a41271f8263bdd8df
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2025-07-03 22:29:13 +0530

    types.fix

--- src/interface/room.interface.ts
+++ src/interface/room.interface.ts
@@ -14,7 +14,7 @@ export interface CancellationTimeline {
   free_cancellation: number;
   no_show: number;
   no_show_description: string | null;
-  free_cancellation_description: string;
+  free_cancellation_description: string | null;
 }
 
 export interface RoomVariant {
@@ -28,6 +28,30 @@ export interface Room {
   room_type_code: string;
   variants_count: number;
   variants: RoomVariant[];
+  images: any | null;
+  properties: any;
+  no_of_adults: any | null;
+  no_of_children: any | null;
+  no_of_total_adults: any | null;
+  no_of_total_children: any | null;
+  children_ages: any | null;
+  passengers_details: any | null;
+  price: any | null;
+  booking_code: any | null;
+  booking_type: any | null;
+  extra_bed: any | null;
+  bedding_preference: any | null;
+  special_requests: any | null;
+  cancellation_status: any | null;
+  cancel_reason: any | null;
+  booking_reference_number: any | null;
+  additional_requests: any | null;
+  additional_services: any | null;
+  supplier_reference: any | null;
+  context: any | null;
+  package_price: any | null;
+  package_cancellation_info: any | null;
+  package_cancellation_timeline: any | null;
 }
 
 export interface RoomBySerialNo {
@@ -47,4 +71,4 @@ export interface RoomCardData {
   cancellationPolicy: string;
   roomTypeCode: string;
   variantsCount: number;
-} 
+}
